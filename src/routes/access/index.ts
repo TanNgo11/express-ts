b6a8010bdc62b6b1fe7544cba81045a7a5f7088a
Tan Ngo <tan_ngo@datahouse.com>
2025-06-30 22:57:03 +0700

    feat(express): error handler

--- src/routes/access/index.ts
+++ src/routes/access/index.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import accessController from "../../controllers/access.controller";
+import { asyncHandler } from "../../auth/checkAuth";
 const router = Router();
 
-router.post("/shop/signup", accessController.signUp);
+router.post("/shop/signup", asyncHandler(accessController.signUp));
 
 export default router;
